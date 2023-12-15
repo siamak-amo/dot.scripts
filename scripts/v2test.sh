3f7080f2042e58d3d76c608edc0152f4ba5b987f
siamak a.m.o <edu.siamak@gmail.com>
2023-12-15 20:56:29 +0330

    completed v2test

--- scripts/v2test.sh
+++ scripts/v2test.sh
@@ -6,24 +6,78 @@
 #   echo LINK | v2test [OPTIONS]
 # 
 # Options:
-#   -co       : keep the generated config file anyway
+#   -co       : keep generated config files anyway
+#   -rc       : don't keep config files anyway
 #
+_V2="v2ray-ng"
+V2=$(which $_V2)
+CURL="$(which curl) -s"
+
+TOUT="5s"
+TEST_API="https://api.ipify.org"
+HT_PROXY="127.0.0.1:10809"
+TEMP_FILENAME="$(date +"%s%M%h%d%m%Y").json"
+
+test_api(){
+    _ip=$(timeout $TOUT $CURL $TEST_API --proxy $HT_PROXY)
+
+    [[ -z $(echo $_ip | grep "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*") ]] \
+        && _RES="Not Working." || _RES="OK."
+}
+
+# $1 is v2ray config link
+mk_ccpath(){
+    CCPATH=$(echo $1 | sha1sum)
+    CCPATH="${CCPATH:0:16}.json"
+}
 
 # if $1 is set, will keep generated json config files
 test_links_stdin(){
-    while IFS=$'\n' read -r v2_link; do
-        sleep 1
-        if [[ 1 == $_keep_config_file ]]; then
-            echo "keep";
-        fi;
-        echo "got $v2_link"
+    while IFS=$'\n' read -r _ln; do
+        mk_ccpath "$_ln"
+        echo "$_ln" | vs2conf > $CCPATH
+        if [[ -n "$(cat $CCPATH)" ]]; then
+            test_config_file "$CCPATH"
+
+            if [[ "OK." == "$_RES" ]]; then
+                echo "$_ln  --  $_RES"
+            else
+                echo "$_RES" >&2
+            fi
+            if [[ 1 == $_keep_config_file || "OK." == "$_RES" ]]; then
+                [[ 1 == $_rm_config_file ]] \
+                    && rm -f $CCPATH || unset CCPATH
+            else
+                rm -f $CCPATH
+            fi
+        else
+            echo "creating config file failed" >&2
+        fi
     done
+}
+
+get_v2_pid(){
+    V2_PID=$(ps -C $_V2 -o pid | tail -1 | grep "[0-9]*" -o)
 }
 
 # $1 is the file path
 test_config_file(){
-    if [[ 1 == $_print_path ]]; then
-        echo $1
+    get_v2_pid
+    if [[ -n "$V2_PID" ]]; then
+        echo "first kill the current running $_V2 instance." >&2
+    else
+        $V2 run -c $1 >/dev/null & 
+        sleep 0.2s
+        get_v2_pid
+        if [[ -z "$V2_PID" ]]; then
+            echo "running $_V2 failed." >&2
+        else
+            test_api
+            if [[ 1 == $_print_path ]]; then
+                printf "%s\t -- %s\n" "$1" "$_RES"
+            fi
+            killall $_V2
+        fi
     fi
 }
 
@@ -39,6 +93,10 @@ else
             ;;
         "-co")
             _keep_config_file=1
+            test_links_stdin
+            ;;
+        "-rc")
+            _rm_config_file=1
             test_links_stdin
             ;;
     esac
