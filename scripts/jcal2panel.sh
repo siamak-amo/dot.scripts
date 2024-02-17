e81b040401e525456f70bb66c12227d4d062e513
siamak a.m.o <edu.siamak@gmail.com>
2024-02-17 12:09:38 +0330

    add support for another mode of printing date

--- scripts/jcal2panel.sh
+++ scripts/jcal2panel.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 #
 #  jcal to panel
-#  output:  چهارشنبه ۳۱ خرداد  [21 Jun]
+#
+#  output:
+#   - MODE 0:  [۳۱ خرداد]
+#   - MODE 1:  چهارشنبه ۳۱ خرداد  [21 Jun]
+#
+[ -z "$MODE" ] && MODE=0
 #
 JDATE=$(which jdate)
 DATE=$(which date)
 
-_j=$($JDATE +%G" "%d" "%V -f)
-_d=$($DATE +%-1e" "%b)
 
-printf "%s  [%s]  " "$_j" "$_d"
+case $MODE in
+    0)
+        _j=$($JDATE +%d" "%V -f)
+        printf " [%s] " "$_j"
+        ;;
+    1)
+        _j=$($JDATE +%G" "%d" "%V -f)
+        _d=$($DATE +%-1e" "%b)
+        printf "%s  [%s]  " "$_j" "$_d"
+        ;;
+    *)
+        printf "???";;
+esac
