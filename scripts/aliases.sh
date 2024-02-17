6c7e10781ad406630ce395470825fb8ae0210e7b
siamak a.m.o <edu.siamak@gmail.com>
2024-02-17 16:55:53 +0330

    add aliases.sh

---
+++ scripts/aliases.sh
@@ -0,0 +1,21 @@
+# aliases.sh
+#
+# Usage:
+#   add the folloing line to your ~/.[your shell]rc
+#   `source /path/to/aliases.sh`
+#
+# General
+alias sl="ls"
+alias mv="mv -i"
+alias cp="cp -i"
+alias ping="timeout 20s ping -c4"
+#
+# Git
+alias gs="git status"
+alias ga="git add"
+alias gaa="git add -a"
+alias gc="git commit"
+alias gcm="git commit -m"
+alias gcl="git clone"
+alias gpush="git push"
+alias gpull="git pull"
