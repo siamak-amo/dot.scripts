18d78de6ad9401c8946722def5957dc6c1019481
siamak a.m.o <edu.siamak@gmail.com>
2024-04-20 11:54:57 +0330

    change defaults to become more like real cowsay

--- scripts/cowsay.sh
+++ scripts/cowsay.sh
@@ -81,8 +81,8 @@ done
 # be careful using the printf special characters,
 # for instance, use %% for a single % character
 [ -z "$HL" ] && HL="-" || HL="${HL:0:1}"
-[ -z "$VL" ] && VL="|" || VL="${VL:0:1}"
-[ -z "$VR" ] && VR="|" || VR="${VR:0:1}"
+[ -z "$VL" ] && VL="<" || VL="${VL:0:1}"
+[ -z "$VR" ] && VR=">" || VR="${VR:0:1}"
 # use AL to set HL and VL at once
 [ -n "$AL" ] && AL="${AL:0:1}" HL=$AL VL=$AL VR=$AL
 # cow's eyes
@@ -90,8 +90,8 @@ done
 # cow's connector
 [ -z "$C" ] && C="\\" || C="${C:0:1}"
 # dialog box corners
-[ -z "$MD" ] && MD="/"  || MD="${MD:0:1}" # main diagonal
-[ -z "$OD" ] && OD="\\" || OD="${OD:0:1}" # other diagonal
+[ -z "$MD" ] && MD=' ' || MD="${MD:0:1}" # main diagonal
+[ -z "$OD" ] && OD=' ' || OD="${OD:0:1}" # other diagonal
 
 cat <<EOF
 $MD$(printf -- "$HL%.0s" $(seq 1 $(($DIALOG_LEN+2))))$OD \
