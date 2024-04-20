3c1c62b14af26ed810a3886cdfeefc8a989a3d7d
siamak a.m.o <edu.siamak@gmail.com>
2024-04-20 16:14:40 +0330

    add doc

--- scripts/mkbackup.sh
+++ scripts/mkbackup.sh
@@ -26,6 +26,10 @@ OPTIONS:
     -p, --prefix              to specify backup path
     -N, --no-nice             to disable nice
     -l, --nice-level          to specify nice -n level
+    --parts                   to specify separated backup parts
+    -s, --solid               to backup in a single file
+    -e, --exclude             to exclude (file/dir) from backup
+    -z, --gzip                to make compressed tar files (tar.gz)
 
 EOF
 }
