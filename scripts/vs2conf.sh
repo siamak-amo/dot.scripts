85460c12febbd352d7e9dce4c6ef88ae9d07b40e
siamak a.m.o <edu.siamak@gmail.com>
2024-04-27 11:36:37 +0330

    set key `tls` to be ignored
    it's value is either empty or 'tls', defiantly should be handled
    somewhere else by considering the value of another parameter,
    for now we just ignore it

--- scripts/vs2conf.sh
+++ scripts/vs2conf.sh
@@ -249,7 +249,7 @@ normalize_kv(){
         "path"|"spx")
             _key="path"
             ;;
-        ""|"port"|"scheme"|"fragment"|"serviceName"|"mode"|"flow"|"headerType"|"v"|"ps")
+        ""|"port"|"scheme"|"fragment"|"serviceName"|"mode"|"flow"|"headerType"|"v"|"ps"|"tls")
             ;;
         *)
             # _key won't affect the final result
