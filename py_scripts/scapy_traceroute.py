9ca6b64f2ee6ea6956eb982e07a024e4978dae26
siamak a.m.o <edu.siamak@gmail.com>
2025-11-18 22:07:38 +0330

    Py_scripts: implement verbose mode

--- py_scripts/scapy_traceroute.py
+++ py_scripts/scapy_traceroute.py
@@ -2,12 +2,15 @@
 
 # finds the route packets trace to network host
 # @return: list of found IPs, '* * *' if not known
-def trr (pac: scapy.layers.inet.IP) ->list[str]:
+def trr (pac: scapy.layers.inet.IP, verbose=False) ->list[str]:
     ips=[]
     maxfail, failures = 5, 0
+    if verbose:
+        print(f"ttl  IP                Duration")
     for t in range (1,42):
         # Set TTL of @pac and send it
         pac['IP'].ttl = t
+        _t0 = time.time()
         res = sr1 (pac, timeout=1, verbose=0)
         if res != None and res.haslayer(IP):
             if res.haslayer(ICMP) and res['ICMP'].type == 3:
@@ -16,10 +19,14 @@ def trr (pac: scapy.layers.inet.IP) ->list[str]:
             failures = 0
             rip = res['IP'].src
             ips.append (rip)
+            if verbose:
+                print(f"{t:<3}  {rip:<15}   {1000*(res.time-_t0):.4f}ms")
             if rip == pac['IP'].dst:
                 break
         else:
             ips.append ('* * *')
+            if verbose:
+                print(f"{t:<3}  * * *")
             failures += 1
             if failures >= maxfail:
                 break
@@ -30,7 +37,7 @@ def trr (pac: scapy.layers.inet.IP) ->list[str]:
 ## Usage Examples ##
 
 # Ping traceroute
-trr( IP(dst='8.8.8.8') / ICMP() )
+trr( IP(dst='8.8.8.8') / ICMP(),  verbose=True )
 
 # DNS traceroute
 ns_req = DNS( qd=[DNSQR(qname='gmail.com')] )
