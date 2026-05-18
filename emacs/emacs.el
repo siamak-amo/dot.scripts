f92d79392e70dcfc4639d64c8ce95b2332c8016d
siamak a.m.o <edu.siamak@gmail.com>
2026-05-18 21:33:41 +0330

    Emacs: fix compilation buffer scrolling

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -163,6 +163,8 @@
   '(define-key compilation-mode-map (kbd "C-c") 'kill-compilation))
 (eval-after-load 'grep
   '(define-key grep-mode-map        (kbd "C-c") 'kill-compilation))
+;;; compilation mode, auto go to end of buffer
+(setq compilation-scroll-output t)
 ;; compilation mode ANSI color
 ;; taken from: <https://stackoverflow.com/a/20788581/12798281>
 (ignore-errors
@@ -171,16 +173,6 @@
     (when (eq major-mode 'compilation-mode)
       (ansi-color-apply-on-region compilation-filter-start (point-max))))
   (add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer))
-;;; compilation mode, auto go to end of buffer
-(add-hook 'compilation-finish-functions
-          (lambda (buffer exit-status)
-            (with-current-buffer buffer
-              (if (string= "finished\n" exit-status)
-                  (let ((compilation-window (get-buffer-window "*compilation*")))
-                    (with-selected-window compilation-window
-                      (goto-char (point-max))
-                      (recenter -1)
-                      ))))))
 ;; dired mode
 (eval-after-load "dired"
   '(progn
