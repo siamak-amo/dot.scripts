f0f7f79e0be830e7f9bd9a57a654fbc6811e048a
siamak a.m.o <edu.siamak@gmail.com>
2026-07-05 13:03:47 +0330

    Emacs: Add join shortcut

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -130,6 +130,7 @@
 (global-set-key (kbd "M-j")       #'windmove-down2)
 (global-set-key (kbd "C-l")       #'scroll-up-line)
 (global-set-key (kbd "C-j")       #'scroll-down-line)
+(global-set-key (kbd "C-S-J")     #'join-line)
 ;; resize window
 (global-set-key (kbd "M-<left>")   'shrink-window-horizontally)
 (global-set-key (kbd "M-<right>")  'enlarge-window-horizontally)
